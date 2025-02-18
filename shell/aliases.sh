bb399f9884a758eded822701a91e86893bcf7e57
Vitalii Sidorok <vitaly@Vitaliis-MacBook-Pro.local>
2025-02-17 19:13:15 -0500

    config rehaul and AI

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -2,3 +2,8 @@ alias la="ls -la"
 alias c="clear"
 alias g="git"
 alias gitsquash='git reset --soft HEAD~$(git rev-list --count HEAD ^master)'
+
+source ~/.env
+# remember to start ssh chanels for socks and tmux ssh separately
+# ALSO turn on VPN to USA = this HELPS immensely
+
